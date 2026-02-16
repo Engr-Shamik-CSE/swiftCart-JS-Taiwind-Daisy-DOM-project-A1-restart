4618b57e7b8681fc1210702fb81718f577d4d9a0
unknown <you@example.com>
2026-02-17 00:53:33 +0600

    Update UI / logic

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+console.log("Bal re Bal");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      navLinks.forEach(l => l.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+});
+
+
